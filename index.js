a726bcbe9ddb8d04ea1fee9fb9c42d366c824f2d
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-06-13 11:01:44 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require('express');
-const WebSocket = require("ws");
+
+import express from 'express';
+import WebSocket from 'ws';
+import puppeteer from 'puppeteer-core';
 const app  = express();
 const port = 3000;
 
-const ws = new WebSocket("wss://brd-customer-hl_b0a0de5f-zone-scraping_browser1:zuh9bm3r1npt@brd.superproxy.io:9222");
 
-ws.on("open", () => console.log("✅ Opened"));
-ws.on("error", (err) => console.log("❌ Error", err.message));
+
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: 'wss://brd-customer-hl_b0a0de5f-zone-scraping_browser1:zuh9bm3r1npt@brd.superproxy.io:9222',
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://www.linkedin.com', { waitUntil: 'domcontentloaded' });
+  console.log('✅ Page loaded:', await page.title());
+
+  await browser.close();
+
 
 app.listen(port,(err)=>{
     if(err)throw err;
